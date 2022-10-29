7ebfee1132943f03972e490f95dfd1a9201be5e2
Alonso Paucar <iaquintanapa@gmail.com>
2022-10-28 19:37:25 -0500

    solucionando el problema

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+// Módulo personalizado
+import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
+
+// Cambiar el local de la app
+import localeEs from '@angular/common/locales/es-PE';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData( localeEs );
+registerLocaleData( localePt );
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRouterModule,
+    VentasModule,
+    SharedModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-PE' }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
